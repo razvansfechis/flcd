a62057c80527a242bed533715a09d5c58b057762
926-Sfechis-Razvan <razvansfechis@yahoo.com>
2023-11-24 01:10:53 +0200

    .

---
+++ lab3/pair.py
@@ -0,0 +1,7 @@
+class Pair:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def __str__(self):
+        return "(" + str(self.first) + "," + str(self.second) + ")"
